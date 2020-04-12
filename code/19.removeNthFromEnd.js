9232754a1104a6d403cba18d3002f8844f8cda8d
oriys <Kirin.Toudou@protonmail.com>
2020-04-12 22:06:37 +0800

    [SOLVE] 19

---
+++ code/19.removeNthFromEnd.js
@@ -0,0 +1,27 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+let removeNthFromEnd = function (head, n) {
+  let fast = head;
+  let slow = head;
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+  if (!fast) {
+    return slow.next;
+  }
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return head;
+};
