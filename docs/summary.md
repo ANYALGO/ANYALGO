dfdbf8747766e75c07b3b727e63fffb1343d7047
CodePoem <codepoetdream@gmail>
2020-04-14 21:38:26 +0800

    feat: add generateSummary

--- docs/summary.md
+++ docs/summary.md
@@ -1,15 +1,15 @@
 - [Home](/)
 
+- [Template](/Template.md)
+
 - LeetCode
 
-  - [模板](/leetcode/Template.md)
   - [猜数字 - 简单](/leetcode/guessNumber.md)
+  - [柠檬水找零 - 简单](/leetcode/lemonadeChange.md)
+  - [亲密字符串 - 简单](/leetcode/buddyStrings.md)
   - [多数元素 - 简单](/leetcode/numJewelsInStones.md)
   - [IP地址无效化 - 简单](/leetcode/defangIPaddr.md)
   - [合并二叉树 - 简单](/leetcode/mergeTrees.md)
-  - [柠檬水找零 - 简单](/leetcode/lemonadeChange.md)
-  - [亲密字符串 - 简单](/leetcode/buddyStrings.md)
-
 - PTA
 
-  - [(3n+1)猜想 - 简单](/PTA/callatzThink.md)
+  - [(3n+1)猜想 - 简单](/leetcode/callatzThink.md)
---
+++ generateSummary.py
@@ -0,0 +1,43 @@
+# python3
+import os
+
+def get_cur_path():
+    import os
+    import sys
+    os.chdir(sys.path[0])
+    return os.getcwd()
+
+def file_name(file_dir):
+    File_Name = []
+    File_Title = []
+    for files in os.listdir(file_dir):
+        if os.path.splitext(files)[1] == '.md':
+            File_Name.append(files)
+            with open(file_dir + os.sep + files, 'r', encoding='utf-8') as f:
+                line = f.readline()
+                line = line.lstrip('#').lstrip(' ').replace('\n', '').replace('\r', '')
+                File_Title.append(line)
+    return File_Name, File_Title
+
+def write_summary(file_dir, leetcode_file_name, leetcode_file_title, pta_file_name, pta_file_title):
+    f = open(file_dir, 'w', encoding='utf-8')
+    content = ['- [Home](/)\n\n']
+    content.append('- [Template](/Template.md)\n\n')
+    content.append('- LeetCode\n\n')
+    for index in range(len(leetcode_file_name)):
+        content.append('  - ['+ leetcode_file_title[index] + '](/leetcode/' + leetcode_file_name[index] +')\n')
+    content.append('- PTA\n\n')
+    for index in range(len(pta_file_name)):
+        content.append('  - ['+ pta_file_title[index] + '](/leetcode/' + pta_file_name[index] +')\n')
+    f.writelines(content)
+    f.close()
+
+if __name__ == '__main__':
+    print("get_cur_path : ", get_cur_path())
+    leetcode_file_name, leetcode_file_title = file_name(os.sep.join([get_cur_path(), 'docs', 'leetcode']))
+    pta_file_name, pta_file_title = file_name(os.sep.join([get_cur_path(), 'docs', 'PTA']))
+    print("leetcode_file_name : ",leetcode_file_name)
+    print("leetcode_file_title : ",leetcode_file_title)
+    print("pta_file_name : ",pta_file_name)
+    print("pta_file_title : ",pta_file_title)
+    write_summary(os.sep.join([get_cur_path(), 'docs', 'summary.md']), leetcode_file_name, leetcode_file_title, pta_file_name, pta_file_title)
