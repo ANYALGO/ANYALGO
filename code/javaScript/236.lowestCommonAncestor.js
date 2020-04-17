6a2a3a55e01204a50c1728baa723751f5931b64e
oriys <Kirin.Toudou@protonmail.com>
2020-04-17 22:47:25 +0800

    [ADD] 236

---
+++ code/javaScript/236.lowestCommonAncestor.js
@@ -0,0 +1,25 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null || q === root || p === root) {
+    return root;
+  }
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) {
+    return root;
+  }
+  if (left || right) {
+    return left || right;
+  }
+  return null;
+};
