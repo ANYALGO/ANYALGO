5cf754294824fc45d5afaec632cee6fc95e660cb
oriys <Kirin.Toudou@protonmail.com>
2020-04-27 22:24:43 +0800

    [ADD] 221

--- code/javaScript/221.maximalSquare.js
+++ code/javaScript/221.maximalSquare.js
@@ -8,12 +8,15 @@ var maximalSquare = function (matrix) {
   }
   let rows = matrix.length;
   let cols = matrix[0].length;
-  let dp = new Array(rows + 1).fill(new Array(cols + 1).fill(0));
+  let dp = [];
+  for (let i = 0; i < rows + 1; i++) {
+    dp.push(new Array(cols + 1).fill(0));
+  }
   let max = 0;
   for (let i = 1; i <= rows; i++) {
     for (let j = 1; j <= cols; j++) {
       if (matrix[i - 1][j - 1] === "1") {
-        dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+        dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1;
         max = Math.max(max, dp[i][j]);
       }
     }
@@ -21,9 +24,11 @@ var maximalSquare = function (matrix) {
   return max ** 2;
 };
 
-maximalSquare([
-  ["1", "0", "1", "0", "0"],
-  ["1", "0", "1", "1", "1"],
-  ["1", "1", "1", "1", "1"],
-  ["1", "0", "0", "1", "0"],
-]);
+console.log(
+  maximalSquare([
+    ["1", "0", "1", "0", "0"],
+    ["1", "0", "1", "1", "1"],
+    ["1", "1", "1", "1", "1"],
+    ["1", "0", "0", "1", "0"],
+  ])
+);
