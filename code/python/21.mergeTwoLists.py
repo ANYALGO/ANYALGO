44f487083ceefe24ff3451eff3e8c43b317fdb2a
oriys <Kirin.Toudou@protonmail.com>
2020-10-05 22:02:13 +0800

    [ADD] 21

---
+++ code/python/21.mergeTwoLists.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val > l2.val:
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = mergeTwoLists(l1, l2.next)
+            return l2
